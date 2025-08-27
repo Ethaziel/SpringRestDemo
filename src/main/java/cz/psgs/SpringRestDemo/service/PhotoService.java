5d6636afa5c07b5eea8afe8ddb6661bddfd6e65b
Petr Slachta <petr.slachta@atlas.cz>
2025-08-27 23:50:37 +0200

    Prod version hotfix - photo clean up from DB

--- src/main/java/cz/psgs/SpringRestDemo/service/PhotoService.java
+++ src/main/java/cz/psgs/SpringRestDemo/service/PhotoService.java
@@ -1,5 +1,7 @@
 package cz.psgs.SpringRestDemo.service;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,9 +10,13 @@ import org.springframework.stereotype.Service;
 
 import cz.psgs.SpringRestDemo.model.Photo;
 import cz.psgs.SpringRestDemo.repository.PhotoRepository;
+import cz.psgs.SpringRestDemo.util.appUtils.AppUtil;
 
 @Service
 public class PhotoService {
+
+    private static final String PHOTOS_FOLDER = "photos";
+    private static final String THUMBNAILS_FOLDER = "thumbnails";
 
     @Autowired
     private PhotoRepository photoRepository;
@@ -29,6 +35,21 @@ public class PhotoService {
 
     public List<Photo> findByAlbumId(long id){
         return photoRepository.findByAlbum_id(id);
+    }
+
+    public void deletePhotoAndFiles(Photo photo) {
+        long albumId = photo.getAlbum().getId();
+        AppUtil.deletePhotoFromPath(photo.getFileName(), PHOTOS_FOLDER, albumId);
+        AppUtil.deletePhotoFromPath(photo.getFileName(), THUMBNAILS_FOLDER, albumId);
+        photoRepository.delete(photo);
+    }
+
+    public void deletePhotoOnlyFromDb(Photo photo) {
+        photoRepository.delete(photo);
+    }
+
+    public List<Photo> findAll() {
+        return photoRepository.findAll();
     }
 
 }
